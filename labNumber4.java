97d09bc94d5cd42c9700220b3c86e2f558f5c829
DeVosha <devoshapatterson@DeVoshas-MacBook-Pro.local>
2019-04-25 08:53:17 -0400

    updated lab4- working

--- labNumber4.java
+++ labNumber4.java
@@ -8,36 +8,27 @@ public class labNumber4 {
 
 		String go = "y";
 
-		// add for loop
-//		while (go.equalsIgnoreCase("Y")) {
-//			System.out.println("Please enter a number");
-		int	num = scan.nextInt();
+		
+		while (go.equalsIgnoreCase("Y")) {
+
+			System.out.println("Please enter a number");
+			int userNum = scan.nextInt();
 			
-			for (num = 1; 1 <= num; num++) {
+
+			for (int num = 1; num <= userNum; num++) {
 				int square = (int) Math.pow(num, 2);
 				int cube = (int) Math.pow(num, 3);
-				System.out.print(square + " ");
-				System.out.println();
-				System.out.println(cube + " ");}
-//				
-//				if ( == "Y") {
-					System.out.println("Would you like to enter another number? Type Y for yes or N for No");
-					go = scan.nextLine();
-//				}
-				
-				scan.close();
+				System.out.print(num + " " + square + " ");
+
+				System.out.println(cube + " ");
 			}
 
+			System.out.println("Would you like to enter another number? Type Y for yes or N for No");
+			go = scan.next();
 		}
-	
-	 
 
+		scan.close();
+	}
 
-//		while (go.equalsIgnoreCase("Y")) {
-//			System.out.println("Would you like to enter another number? Type Y for yes or N for No");
-//	go = scan.nextLine();
-//	System.out.println("Thank you for your participation!");
-//		}
-//			
-//			scan.close();
-//	}
+}
+
